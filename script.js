085e718b680a5f7f653350d9b3f05e117bc9fdcc
Olive <golyo278@newschool.edu>
2019-10-24 02:34:10 +0800

    no hover mobile

--- script.js
+++ script.js
@@ -5,13 +5,44 @@ function myFunction() {
 }
 
 
+function hasTouch() {
+    return 'ontouchstart' in document.documentElement
+           || navigator.maxTouchPoints > 0
+           || navigator.msMaxTouchPoints > 0;
+}
 
+if (hasTouch()) { // remove all :hover stylesheets
+    try { // prevent exception on browsers not supporting DOM styleSheets properly
+        for (var si in document.styleSheets) {
+            var styleSheet = document.styleSheets[si];
+            if (!styleSheet.rules) continue;
+
+            for (var ri = styleSheet.rules.length - 1; ri >= 0; ri--) {
+                if (!styleSheet.rules[ri].selectorText) continue;
+
+                if (styleSheet.rules[ri].selectorText.match(':hover')) {
+                    styleSheet.deleteRule(ri);
+                }
+            }
+        }
+    } catch (ex) {}
+}
 
 // $(".dropbtn").click(function() {
 // $(".content").toggle();
 // });
 
-$(".dropbtn").click(function() {
+$(".dropdown").click(function() {
 $(".black").toggle();
-$("body").css("overflow");
+
+
 });
+
+
+// $(".dropdown").click(function() {
+// $("body").css("overflow", "hidden");
+// });
+
+// $(".dropbtn").click(function() {
+// $("body").css("overflow", "initial");
+// });
