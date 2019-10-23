a362e0483d9dbc615db9ab94e4190fc857e2a9ae
Olive <golyo278@newschool.edu>
2019-10-23 23:52:54 +0800

    mobile nav

---
+++ script.js
@@ -0,0 +1,16 @@
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+
+
+
+// $(".dropbtn").click(function() {
+// $(".content").toggle();
+// });
+
+$(".dropbtn").click(function() {
+$(".black").toggle();
+});
